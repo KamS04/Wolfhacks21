4843a9c8cd3c5a8e87222f402a0225b10e27721b
Kam S <singhkam07@mail.com>
2021-04-11 10:39:08 -0400

    Added main and ignored .DS_STORE

---
+++ source/main.py
@@ -0,0 +1,13 @@
+class Application():
+    def __init__(self):
+        # Here shall be the code that initializes global variables
+        pass
+
+    def run(self):
+        # Hera shall be where we start the app
+        pass
+
+
+if __name__ == '__main__':
+    application = Application()
+    application.run()
